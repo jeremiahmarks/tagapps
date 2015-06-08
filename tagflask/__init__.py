be04ec6695ea2f50779bee36b9827804ebbfbe93
Jeremiah Marks <Jeremiah@JL Marks.org>
2015-06-08 08:36:59 +0000

    end of the night commit

--- tagflask/__init__.py
+++ tagflask/__init__.py
@@ -1,26 +1,49 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
-
+from datetime import datetime
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
-bootstrap=Bootstrap(app)
+app.config['SECRET_KEY'] = 'isthisrandom'
+## Todo:  move this from the actual code to an environment variable
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
-@app.route("/")
-def hello():
-	return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
 
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'),404
 
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('500.html'), 500
+
+
 
 if __name__ == "__main__":
 	app.run()
